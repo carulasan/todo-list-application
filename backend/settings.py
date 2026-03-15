18659d54355bd59b7bdceec4b6435434fb873c47
Brylle Carulasan <bryllecarulasan@YONDU105275.local>
2026-03-16 07:03:34 +0800

    update settings

--- backend/settings.py
+++ backend/settings.py
@@ -6,10 +6,10 @@ from pydantic import BaseModel
 class SystemSettings(BaseModel):
     """Initial System Settings loaded from ENV."""
 
-    ENV: str = None
+    ENV: str = "Development"
     DEBUG_MODE: Optional[str] = "false"
     ALLOWED_HOST: str = "*"
-    CONFIG_NAME: str = "Geoplan-API"
+    CONFIG_NAME: str = "Geoplan-API-Final"
 
 
     class Config:
@@ -22,14 +22,14 @@ class SystemSettings(BaseModel):
 class DatabaseClientSettings(BaseModel):
     """Database related settings whether from PostgreSql, Redis, etc.."""
 
-    REDIS_HOST: Optional[str] = None
+    REDIS_HOST: Optional[str] = "default:gQAAAAAAARqdAAIncDJkZWQxMWQ2OTA3ZWE0YThlOGRmZjU2ODNkNzg3Njg4ZXAyNzIzNDk@complete-sculpin-72349.upstash.io"
     REDIS_PORT: Optional[str] = '6379'
     REDIS_DB: Optional[str] = '0'
     DATABASE_ENGINE_POOL_SIZE: Optional[int] = 10
     DATABASE_ENGINE_MAX_OVERFLOW: Optional[int] = 10
     DATABASE_ENGINE_POOL_PING: Optional[int] = 0
-    READ_DATABASE_URL: Optional[str] = None
-    WRITE_DATABASE_URL: Optional[str] = None
+    READ_DATABASE_URL: Optional[str] = "postgresql://todo_ee1b_user:umPdgcgkWqMEZYO82PbCQ6YhNXvwgcRU@dpg-d6rblfvgi27c73agjk3g-a.oregon-postgres.render.com:5432/todo_ee1b"
+    WRITE_DATABASE_URL: Optional[str] = "postgresql://todo_ee1b_user:umPdgcgkWqMEZYO82PbCQ6YhNXvwgcRU@dpg-d6rblfvgi27c73agjk3g-a.oregon-postgres.render.com:5432/todo_ee1b"
     ENABLE_DB_LOGGING: Optional[str] = "false"
 
     @property
